379dceb737c8953e5db1393c63537dd70e5dccce
malithk911 <78416064+malithk911@users.noreply.github.com>
2021-05-15 13:38:52 +0530

    Committed whole project

--- app/utils/ApplicationUtil.java
+++ app/utils/ApplicationUtil.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import play.libs.Json;
 
 public class ApplicationUtil {
-
+    
     public static ObjectNode createResponse(Object response, boolean ok) {
         ObjectNode result = Json.newObject();
         result.put("status", ok);
